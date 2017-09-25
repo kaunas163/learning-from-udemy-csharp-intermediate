6eb240658d9346df2047fd0f4a03b0354275773c
Ieva <ieva.jaksaityte@gmail.com>
2017-09-25 21:06:03 +0300

    section 3 code examples

---
+++ 3-2 composition/Installer.cs
@@ -0,0 +1,17 @@
+﻿namespace _3_2_composition
+{
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("We are installing the application.");
+        }
+    }
+}
---
+++ 3-2 composition/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3_2_composition
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
