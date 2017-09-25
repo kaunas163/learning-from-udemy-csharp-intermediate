62311cb5bce6bb95ef1dba50d88d6001baac49ea
Ieva <ieva.jaksaityte@gmail.com>
2017-09-25 20:30:32 +0300

    stopwatch excersise

---
+++ 2-10 exercise 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_10_exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            Console.WriteLine("Write 'start' when you want to start the stopwatch");
+            Console.WriteLine("And write 'stop' when you want to stop the stopwatch");
+            string input;
+            do
+            {
+                // TODO: let it run multiple times, but better than this
+
+                input = Console.ReadLine();
+                if (input == "start")
+                {
+                    stopwatch.Start();
+                }
+                input = Console.ReadLine();
+                if (input == "stop")
+                {
+                    stopwatch.Stop();
+                }
+                Console.WriteLine("Again? (y/n)");
+                input = Console.ReadLine();
+            } while (input == "y");
+        }
+    }
+}
---
+++ 2-10 exercise 1/Stopwatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2_10_exercise_1
+{
+    public class Stopwatch
+    {
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public TimeSpan Duration { get; private set; }
+
+        public void Start()
+        {
+            // TODO: don't let to start stopwatch if it's already started
+            StartTime = DateTime.Now;
+            Console.WriteLine("Stopwatch started: " + StartTime);
+        }
+
+        public void Stop()
+        {
+            EndTime = DateTime.Now;
+            Console.WriteLine("Stopwatch stopped: " + EndTime);
+            Duration = EndTime - StartTime;
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Stopwatch duration: " + Duration);
+        }
+    }
+}
