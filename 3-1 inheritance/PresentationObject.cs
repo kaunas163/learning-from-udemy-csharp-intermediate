db5d234301ee70d7a733af5ffa9a70db962553bc
Ieva <ieva.jaksaityte@gmail.com>
2017-09-25 20:54:56 +0300

    section 3 code examples

---
+++ 3-1 inheritance/PresentationObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3_1_inheritance
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard.");
+        }
+
+        public void Dublicate()
+        {
+            Console.WriteLine("Ojbect was dublicated.");
+        }
+    }
+}
